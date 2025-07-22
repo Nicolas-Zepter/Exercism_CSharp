1d2c81b43fbe29fb6e5f8ca49d6dc841001b89cb
Nicolas Zepter <nzepter@gmail.com>
2025-07-22 12:26:23 +0200

    Add "Gigasecond" exercise.

---
+++ Exercism_CSharp/Gigasecond.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercism_CSharp
+{
+    #region Task Description
+    /*
+        Your task is to determine the date and time one gigasecond after a certain date.
+        A gigasecond is one thousand million seconds. That is a one with nine zeros after it.
+        If you were born on January 24th, 2015 at 22:00 (10:00:00pm), then you would be a gigasecond old on October 2nd, 2046 at 23:46:40 (11:46:40pm).
+    */
+    #endregion
+
+    public static class Gigasecond
+    {
+        /// <summary>
+        /// Determines the DateTime one gigasecond after a given moment.
+        /// </summary>
+        /// <param name="moment">The initial moment.</param>
+        /// <returns>A DateTime one gigasecond after moment.</returns>
+        public static DateTime Add(DateTime moment) => moment.AddSeconds(1000000000);
+    }
+}
--- Exercism_CSharp/Program.cs
+++ Exercism_CSharp/Program.cs
@@ -1,10 +1,17 @@
 ﻿using Exercism_CSharp;
 
 #region Testing Allergies
-
+/*
 Allergies allergies = new(257);
 Console.WriteLine(allergies.IsAllergicTo(Allergen.Peanuts));
 Console.WriteLine(allergies.IsAllergicTo(Allergen.Eggs));
 Console.WriteLine(string.Join(",", allergies.List()));
+*/
+#endregion
+
+#region Testing Gigasecond
+
+DateTime test = DateTime.Now;
+Console.WriteLine(Gigasecond.Add(test));
 
 #endregion
