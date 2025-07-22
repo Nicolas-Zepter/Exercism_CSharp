26b8d9f770bcf2d5dc3ead94d933b91adab23950
Nicolas Zepter <nzepter@gmail.com>
2025-07-22 11:27:22 +0200

    Initial commit of Exercism_CSharp

---
+++ Exercism_CSharp/Allergies.cs
@@ -0,0 +1,69 @@
+﻿namespace Exercism_CSharp
+{
+    #region Task Description
+
+    /*
+     Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies.
+     An allergy test produces a single numeric score which contains the information about all the allergies the person has (that they were tested for).
+     The list of items (and their value) that were tested are:
+
+    eggs (1)
+    peanuts (2)
+    shellfish (4)
+    strawberries (8)
+    tomatoes (16)
+    chocolate (32)
+    pollen (64)
+    cats (128)
+
+     So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
+     Now, given just that score of 34, your program should be able to say:
+     Whether Tom is allergic to any one of those allergens listed above.
+     All the allergens Tom is allergic to.
+     Note: a given score may include allergens not listed above (i.e. allergens that score 256, 512, 1024, etc.). 
+     Your program should ignore those components of the score. For example, if the allergy score is 257, your program should only report the eggs (1) allergy.
+    */
+
+    #endregion
+
+    [Flags]
+    public enum Allergen
+    {
+        Eggs = 1,
+        Peanuts = 2,
+        Shellfish = 4,
+        Strawberries = 8,
+        Tomatoes = 16,
+        Chocolate = 32,
+        Pollen = 64,
+        Cats = 128
+    }
+
+    public class Allergies
+    {
+        #region Fields
+
+        private int _mask;
+
+        #endregion
+
+        #region Init
+
+        public Allergies(int mask)
+        {
+            _mask = mask;
+        }
+
+        #endregion
+
+        #region Methods
+        public bool IsAllergicTo(Allergen allergen) => (_mask & (int)allergen) != 0;
+
+        public Allergen[] List() => Enum.GetValues(typeof(Allergen))
+                                        .Cast<Allergen>()
+                                        .Where(a => (_mask & (int)a) != 0)
+                                        .ToArray();
+
+        #endregion
+    }
+}
---
+++ Exercism_CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Exercism_CSharp;
+
+#region Testing Allergies
+
+Allergies allergies = new(257);
+Console.WriteLine(allergies.IsAllergicTo(Allergen.Peanuts));
+Console.WriteLine(allergies.IsAllergicTo(Allergen.Eggs));
+Console.WriteLine(string.Join(",", allergies.List()));
+
+#endregion
