6078829597e8725860809bab8fd50cc8c0ebb128
Nicolas Zepter <nzepter@gmail.com>
2025-07-28 19:23:27 +0200

    Add exercise "Pangram".

---
+++ Exercism_CSharp/Pangram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism_CSharp
+{
+    #region Task Description
+    /*
+    Your task is to figure out if a sentence is a pangram.
+    A pangram is a sentence using every letter of the alphabet at least once. It is case insensitive, so it doesn't matter if a letter is lower-case (e.g. k) or upper-case (e.g. K).
+    For this exercise, a sentence is a pangram if it contains each of the 26 letters in the English alphabet.
+    */
+    #endregion
+
+    public static class Pangram
+    {
+        public static bool IsPangram(string input) =>
+           input.Where(Char.IsLetter)
+                .Select(Char.ToLowerInvariant)
+                .Distinct()
+                .Count() == 26;
+    }
+}
